ebdb11d8108067909168ec63b5bfab3748c8a2d1
Amanda Walker <acwalkerca88@gmail.com>
2018-07-14 09:58:50 -0400

    complete

---
+++ config.py
@@ -0,0 +1,5 @@
+# Twitter API Keys
+consumer_key = "PlFMvvTsYViekGqjhSotU7dMY"
+consumer_secret = "33KiK2Pt30eTzSORh0cqXDlV5yC9BEtL2lLted5mvc7ztB3zJK"
+access_token = "1013063133798297600-a9Uipmrudck1AYyHoxGqRbSoPLg303"
+access_token_secret = "4Zxyt1R0o3f2Z40CQKacRh7S60Y3ifYIT2Sz35zqg7ZED"
